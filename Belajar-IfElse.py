283f9afaa1f6fb32d1a2937210fe2bac54cbf924
Dany Candra Febrianto <mrdanydance@gmail.com>
2019-04-05 13:33:23 +0700

    File List Selesai

---
+++ Belajar-IfElse.py
@@ -0,0 +1,32 @@
+nilai = 75
+
+if nilai == 75:
+    print(nilai)
+
+if nilai is 75:
+    print(nilai)
+
+
+if 80<= nilai <=100:
+    print('Nilai Anda A')
+elif 70 <= nilai <80:
+    print('Nilai Anda B')
+elif 60 <= nilai < 70:
+    print('Nilai Anda C')
+elif 50 <= nilai < 60:
+    print('Nilai Anda D')
+else:
+    print('Engga Lulus Gan')
+
+gorengan=["bakwan","cireng","Tahu"]
+beli="Tahu"
+
+if beli in gorengan:
+    print('Saya Jual '+beli)
+
+if not beli in gorengan:
+    print('Saya ga jual'+beli)
+
+
+
+
