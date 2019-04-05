ac0cc83d654d6a237d2d59ef23260b8ac5a3f94c
Dany Candra Febrianto <mrdanydance@gmail.com>
2019-04-05 11:12:12 +0700

    File Number Selesai

---
+++ Number.py
@@ -0,0 +1,27 @@
+a=1
+b=2
+c=10
+
+#penjumlahan
+print(a+b)
+
+#pengurangan
+print(c-a)
+
+#perkalian
+print(c*a)
+
+#pembagian
+print(c/b)
+
+#pembagian bil bulat
+print(c//b)
+
+#modulus
+print(c%b)
+
+#pangkat
+print(c**b)
+
+#penjumlahan bertingkat
+print((a+b)*c)
