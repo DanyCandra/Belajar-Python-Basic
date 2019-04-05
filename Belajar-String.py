ab1b610fcfcf425c3ba7e1033eee015076e1e497
Dany Candra Febrianto <mrdanydance@gmail.com>
2019-04-05 11:29:48 +0700

    File String Selesai

---
+++ Belajar-String.py
@@ -0,0 +1,27 @@
+text='Coba String'
+
+text2='Jalan Jalan di hari jum\'at'
+
+text3="Jalan Jalan di Hari Jum'at"
+
+text4="Dany Berkata \"Kemarin Kemana??\" "
+
+text5="Dany Berkata : \n\"Hal0 Apa Kabar..?\""
+
+text6="""
+    Dany    : "Apa Kabar...?"
+    Candra  : "Sehat Bro..."
+"""
+
+text7= r'C:\nyoto'
+text8=5*'Ha'
+
+text9="Dany"+"Candra"
+
+#substring
+text="Pisang Goreng"
+a=text[0:6]
+b=text[-4]
+
+print(b)
+
