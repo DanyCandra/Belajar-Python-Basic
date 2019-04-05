d3456ae1f04e204034ea0cc7f5b36dbf6654bd86
Dany Candra Febrianto <mrdanydance@gmail.com>
2019-04-05 15:01:08 +0700

    Commit Sampai Function

---
+++ Belajar-FunctinReturn.py
@@ -0,0 +1,18 @@
+def kuadrat(arg):
+    total=arg**2
+    return total
+
+print(kuadrat(2))
+
+def tambah(arg1,arg2):
+    total=arg1+arg2
+    return  total
+
+def kali(arg1,arg2):
+    total=arg1*arg2
+    return total
+
+a=tambah(1,2)
+b=kali(1,a)
+
+print(b)
---
+++ Belajar-FunctionArgument.py
@@ -0,0 +1,12 @@
+def guru(nama,pelajaran):
+    print('Guru ini Bernama : ',nama)
+    print('Mengajar : ',pelajaran)
+
+def penjagaSekolah(nama,shift='siang',galak='tidak'):
+    print('Penjaga ini Bernama : ', nama)
+    print('Shift : ', shift)
+    print('Galak : ', galak)
+
+penjagaSekolah('Sapto')
+penjagaSekolah('Soleh',shift='Malam')
+penjagaSekolah('Dirwan',galak='Banget')
